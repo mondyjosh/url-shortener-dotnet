b5add223a79d01b048eca2351750cf8700b13a41
Josh Mondy <55757183+mondyjosh@users.noreply.github.com>
2025-02-22 19:29:26 -0600

    WIP: Got secrets and environment behaving.

--- db/init/roles.sql
+++ db/init/roles.sql
@@ -9,7 +9,8 @@ WITH
 -- Grant connect privilege to healthcheck role
 GRANT 
     CONNECT 
-ON DATABASE postgres
+ON
+DATABASE postgres
 TO healthcheck;
 
 -- Create login role for Flyway migrations with database and role creation privileges
@@ -17,59 +18,69 @@ CREATE USER migrations_flyway
 WITH
     CREATEDB 
 	CREATEROLE
-	PASSWORD 'development';
+	PASSWORD 'development_migrations';
 
--- -- Create login role for LinksApi.Web backend with default permissions
--- CREATE USER backend_linksapiweb;
+-- Create login role for LinksApi.Web backend with default permissions
+CREATE USER backend_linksapiweb
+	PASSWORD 'development_backend';
 
--- -- Grant necessary permissions to backend role in the public schema
--- GRANT
---     SELECT,
---     INSERT,
---     UPDATE,
---     DELETE 
--- ON ALL tables IN SCHEMA public 
--- TO backend_linksapiweb;
+-- Grant necessary permissions to backend role in the public schema
+GRANT
+    SELECT,
+	INSERT,
+	UPDATE,
+	DELETE
+	ON ALL TABLES IN SCHEMA public 
+to backend_linksapiweb;
 
--- -- Ensure future tables grant the same permissions to backend role by default
--- ALTER DEFAULT PRIVILEGES 
--- GRANT
---     SELECT,
---     INSERT,
---     UPDATE,
---     DELETE 
--- ON TABLES 
--- TO backend_linksapiweb;
+-- Ensure future tables grant the same permissions to backend role by default
+ALTER DEFAULT PRIVILEGES 
+GRANT
+    SELECT,
+	INSERT,
+	UPDATE,
+	DELETE
+	ON TABLES 
+TO backend_linksapiweb;
 
--- -- Grant sequence usage for auto-incrementing fields to backend role
--- GRANT 
---     USAGE,
---     SELECT
--- ON ALL SEQUENCES IN SCHEMA public 
--- TO backend_linksapiweb;
+-- Grant sequence usage for auto-incrementing fields to backend role
+GRANT 
+    USAGE,
+    SELECT
+	ON ALL SEQUENCES IN SCHEMA public 
+TO backend_linksapiweb;
 
--- -- Ensure future sequences grant the same permissions to backend role by default
--- ALTER DEFAULT PRIVILEGES 
--- GRANT
---     USAGE,
---     SELECT
--- ON SEQUENCES 
--- TO backend_linksapiweb;
+-- Ensure future sequences grant the same permissions to backend role by default
+ALTER DEFAULT PRIVILEGES 
+GRANT
+    USAGE,
+    SELECT
+	ON SEQUENCES 
+TO backend_linksapiweb;
 
--- REVOKE GRANT OPTION FOR ALL PRIVILEGES ON ALL TABLES IN SCHEMA public
--- FROM backend_linksapiweb;
+-- Revoke option for backend role by default
+REVOKE GRANT OPTION 
+FOR ALL PRIVILEGES 
+ON ALL TABLES IN SCHEMA public
+FROM backend_linksapiweb;
 
--- ALTER DEFAULT PRIVILEGES REVOKE GRANT OPTION FOR ALL PRIVILEGES ON TABLES
--- FROM backend_linksapiweb;
+--  Ensure future sequences revoke option for backend role by default
+ALTER DEFAULT PRIVILEGES 
+REVOKE GRANT OPTION 
+FOR ALL PRIVILEGES ON TABLES
+FROM backend_linksapiweb;
 
--- Revoke the ability to create tables for all users
+-- Revoke create tables privilege for all users
 REVOKE 
     CREATE 
-ON SCHEMA public
-FROM PUBLIC;
+ON
+SCHEMA public
+FROM
+PUBLIC;
 
--- Grant back the ability to create tables to Flyway migrations role
+-- Restore create tables privilege to Flyway migrations role
 GRANT 
     CREATE 
-ON SCHEMA public 
+ON
+SCHEMA public 
 TO migrations_flyway;
